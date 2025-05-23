5f77e48579728243d807f30e1e596e6672c9f4e2
Y-Sravanthi5 <kvssnpk22@gmail.com>
2025-05-23 10:59:48 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,33 @@
+// Java program to demonstrate working of method
+// overloading in Java
+public class MethodOverloading {
+    
+    // Overloaded sum() 
+    // This sum takes two int parameters
+    public int sum(int x, int y) { return (x + y); }
+
+    // Overloaded sum()
+    // This sum takes three int parameters
+    public int sum(int x, int y, int z)
+    {
+        return (x + y + z);
+    }
+
+    // Overloaded sum() 
+    // This sum takes two double parameters
+    public double sum(double x, double y)
+    {
+        return (x + y);
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        MethodOverloading M = new MethodOverloading();
+        System.out.println(M.sum(10, 20));
+        System.out.println(M.sum(10, 20, 30));
+        System.out.println(M.sum(10.5, 20.5));
+    }
+}
+
+
